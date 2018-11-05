3d06a1eecdbfd502dd583276f659e879c64208e7
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-11-05 17:03:39 -0400

    - Mejoras de UX - Animacion y carga de Feed de instagram - Comparte aplicaicon - Acceso dircto a linux dominicana y contacto del OS

--- lib/home_body.dart
+++ lib/home_body.dart
@@ -1,38 +1,141 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'dart:core';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+import 'dart:convert';
+import 'package:async_loader/async_loader.dart';
 
-class HomeBody extends StatelessWidget{
+class MediaObjects {
+  MediaObjects({
+    @required this.text,
+    @required this.imageLink,
+  });
+
+  final text;
+  final imageLink;
+}
+
+class HomeBody extends StatefulWidget {
+  @override
+  State createState() => _HomeBody();
+}
+
+class _HomeBody extends State<HomeBody> {
+  var _datList = [];
+  final GlobalKey<AsyncLoaderState> _asyncLoaderState =
+      new GlobalKey<AsyncLoaderState>();
+
+  @override
+  void initState() {
+    super.initState();
+//    getImages();
+  }
+
+  Future<List<MediaObjects>> getImages() async {
+    List<MediaObjects> miLista = [];
+    var res = await http.get("https://www.instagram.com/opensaturday/?_a=1");
+    var pars = parse(res.body);
+    print(pars.body.children[1].firstChild.toString().substring(21));
+    var str = pars.body.children[1].firstChild.toString();
+    var obj = jsonDecode(str.substring(21, str.length - 2));
+    var lista = obj['entry_data']['ProfilePage'][0]['graphql']['user']
+        ['edge_owner_to_timeline_media']['edges'];
+    lista.forEach((el) {
+      miLista.add(MediaObjects(
+          text: el['node']['edge_media_to_caption']['edges'][0]['node']['text'],
+          imageLink: el['node']['thumbnail_resources'][3]['src']));
+//        print(el['node']['thumbnail_resources'][3]['src']);
+//        print(el['node']['edge_media_to_caption']['edges'][0]['node']['text']);
+    });
+    setState(() {
+      _datList = miLista;
+    });
+    return miLista;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              backgroundColor: Colors.black,
-              flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text("OpenSaturday",
+    return new AsyncLoader(
+      key: _asyncLoaderState,
+      initState: () async => await getImages(),
+      renderLoad: () => Scaffold(
+          appBar: new AppBar(),
+          body: Container(
+              child: Center(
+                  child: new CircularProgressIndicator(
+            backgroundColor: Colors.blue,
+          )))),
+      renderError: ([error]) =>
+          new Text('Sorry, there was an error loading your joke'),
+      renderSuccess: ({data}) => CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                backgroundColor: Colors.black,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text("OpenSaturday",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    background: Padding(
+                      padding: const EdgeInsets.all(44.0),
+                      child: Image.asset(
+                        "images/logo.png",
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+              ),
+              new SliverList(
+                  delegate: SliverChildListDelegate(<Widget>[
+                InkWell(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Un evento que busca promover herramientas, tecnología y conocimientos open source vanguardistas, a través de profesionales del área.',
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  background: Padding(
-                    padding: const EdgeInsets.all(44.0),
-                    child: Image.asset(
-                      "images/logo.png",
-                      fit: BoxFit.cover,
+                          fontSize: 24.0, fontWeight: FontWeight.normal),
+                      textAlign: TextAlign.center,
                     ),
-                  )),
-            ),
-          ];
-        },
-      body: Center(
-        child: Text("Sample Text"),
-      ),
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                  ),
+                )
+              ])),
+              new SliverGrid(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200.0,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  childAspectRatio: 1.0,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      color: Colors.teal[100 * (index % 9)],
+                      child: Stack(children: <Widget>[
+                        Image.network(_datList[index].imageLink),
+                        Align(
+                            alignment: Alignment(0.0, 1.0),
+                            child: Container(
+                                color: Colors.black87,
+                                child: Text(_datList[index].text)))
+                      ]),
+                    );
+                  },
+                  childCount: _datList.length,
+                ),
+              ),
+            ],
+          ),
     );
   }
 }
--- lib/home_schedule.dart
+++ lib/home_schedule.dart
@@ -3,17 +3,21 @@ import 'package:opensaturday/services/schedulefactory.dart';
 import 'package:opensaturday/services/scheduleservice.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/animation.dart';
 
 class HomeSchedule extends StatefulWidget {
   @override
   State createState() => _HomeSchedule();
 }
 
-class _HomeSchedule extends State<HomeSchedule> {
+class _HomeSchedule extends State<HomeSchedule> with SingleTickerProviderStateMixin{
   Map days;
   ScheduleService srv = ScheduleFactory();
   List<Tab> tabs = List();
   List<ScheduleList> schedule = List();
+
+  Animation<double> animation;
+  AnimationController controller;
 
   @override
   void initState() {
@@ -23,35 +27,53 @@ class _HomeSchedule extends State<HomeSchedule> {
 //          icon: Icon(Icons.schedule),
         )));
     days.forEach((k, v) => schedule.add(ScheduleList(schedule: v)));
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 300), vsync: this);
+    animation = Tween(begin: 0.1, end: 1.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          // the state that has changed here is the animation object’s value
+        });
+      });
+    controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 //      appBar: new TabBar(tabs: tabs),
-      body: DefaultTabController(
-          length: tabs.length == 0 ? 0 : tabs.length,
-          child: Scaffold(
-            backgroundColor: Colors.black87,
-            appBar: AppBar(
-              title: Text('Agenda'),
-              centerTitle: true,
-              bottom: PreferredSize(
-                preferredSize: Size.fromHeight(20.0),
-                child: TabBar(
+      body: Opacity(
+        opacity: animation.value,
+        child: DefaultTabController(
+            length: tabs.length == 0 ? 0 : tabs.length,
+            child: Scaffold(
+              backgroundColor: Colors.black87,
+              appBar: AppBar(
+                title: Text('Agenda'),
+                centerTitle: true,
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(20.0),
+                  child: TabBar(
 //                  isScrollable: true,
-                    tabs: tabs
+                      tabs: tabs
+                  ),
                 ),
               ),
-            ),
-            body: TabBarView(
-                children: schedule
-            ),
+              body: TabBarView(
+                  children: schedule
+              ),
 
-          )
+            )
 
+        ),
       ),
 
     );
+  }
+
+  dispose() {
+    controller.reverse();
+    controller.dispose();
+    super.dispose();
   }
 }
