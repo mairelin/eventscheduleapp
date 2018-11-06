f2a907b41b024c9f4612a1d15cbf599cce4827f0
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-11-05 22:52:53 -0400

    - Cambio de icono - Hero animation
    Signed-off-by: Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>

--- lib/home_body.dart
+++ lib/home_body.dart
@@ -9,12 +9,16 @@ import 'package:async_loader/async_loader.dart';
 
 class MediaObjects {
   MediaObjects({
+    @required this.tag,
     @required this.text,
     @required this.imageLink,
+    @required this.imageSrc,
   });
 
+  final tag;
   final text;
   final imageLink;
+  final imageSrc;
 }
 
 class HomeBody extends StatefulWidget {
@@ -37,15 +41,20 @@ class _HomeBody extends State<HomeBody> {
     List<MediaObjects> miLista = [];
     var res = await http.get("https://www.instagram.com/opensaturday/?_a=1");
     var pars = parse(res.body);
-    print(pars.body.children[1].firstChild.toString().substring(21));
+//    print(pars.body.children[1].firstChild.toString().substring(21));
     var str = pars.body.children[1].firstChild.toString();
     var obj = jsonDecode(str.substring(21, str.length - 2));
     var lista = obj['entry_data']['ProfilePage'][0]['graphql']['user']
         ['edge_owner_to_timeline_media']['edges'];
+    var counter = 0;
     lista.forEach((el) {
       miLista.add(MediaObjects(
+        tag: '$counter',
           text: el['node']['edge_media_to_caption']['edges'][0]['node']['text'],
-          imageLink: el['node']['thumbnail_resources'][3]['src']));
+          imageLink: el['node']['thumbnail_resources'][3]['src'],
+          imageSrc: el['node']['thumbnail_src']
+      ));
+      counter++;
 //        print(el['node']['thumbnail_resources'][3]['src']);
 //        print(el['node']['edge_media_to_caption']['edges'][0]['node']['text']);
     });
@@ -84,10 +93,14 @@ class _HomeBody extends State<HomeBody> {
                           fontSize: 16.0,
                         )),
                     background: Padding(
-                      padding: const EdgeInsets.all(44.0),
+                      padding: const EdgeInsets.only(
+                          left: 30.0,
+                        top: 25.0,
+                        right: 30.0
+                      ),
                       child: Image.asset(
                         "images/logo.png",
-                        fit: BoxFit.cover,
+                        fit: BoxFit.fitWidth,
                       ),
                     )),
               ),
@@ -118,17 +131,35 @@ class _HomeBody extends State<HomeBody> {
                 ),
                 delegate: SliverChildBuilderDelegate(
                   (BuildContext context, int index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      color: Colors.teal[100 * (index % 9)],
-                      child: Stack(children: <Widget>[
-                        Image.network(_datList[index].imageLink),
-                        Align(
-                            alignment: Alignment(0.0, 1.0),
-                            child: Container(
-                                color: Colors.black87,
-                                child: Text(_datList[index].text)))
-                      ]),
+                    return GestureDetector(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute<void>(
+                            builder: (BuildContext context) {
+                              return Scaffold(
+                                appBar: AppBar(
+                                    title: Text('Imagen')
+                                ),
+                                body: SizedBox.expand(
+                                  child: GridPhotoViewer(photo: _datList[index]),
+                                ),
+                              );
+                            }
+                        ));
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Colors.teal[100 * (index % 9)],
+                        child: Stack(children: <Widget>[
+                          Hero(
+                            tag:_datList[index].tag,
+                              child: Image.network(_datList[index].imageSrc)),
+                          Align(
+                              alignment: Alignment(0.0, 1.0),
+                              child: Container(
+                                  color: Colors.black87,
+                                  child: Text(_datList[index].text)))
+                        ]),
+                      ),
                     );
                   },
                   childCount: _datList.length,
@@ -136,6 +167,107 @@ class _HomeBody extends State<HomeBody> {
               ),
             ],
           ),
+    );
+  }
+}
+
+const double _kMinFlingVelocity = 800.0;
+class GridPhotoViewer extends StatefulWidget {
+  const GridPhotoViewer({ Key key, this.photo }) : super(key: key);
+
+  final MediaObjects photo;
+
+  @override
+  _GridPhotoViewerState createState() => _GridPhotoViewerState();
+}
+
+class _GridPhotoViewerState extends State<GridPhotoViewer> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _flingAnimation;
+  Offset _offset = Offset.zero;
+  double _scale = 1.0;
+  Offset _normalizedOffset;
+  double _previousScale;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this)
+      ..addListener(_handleFlingAnimation);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // The maximum offset value is 0,0. If the size of this renderer's box is w,h
+  // then the minimum offset value is w - _scale * w, h - _scale * h.
+  Offset _clampOffset(Offset offset) {
+    final Size size = context.size;
+    final Offset minOffset = Offset(size.width, size.height) * (1.0 - _scale);
+    return Offset(offset.dx.clamp(minOffset.dx, 0.0), offset.dy.clamp(minOffset.dy, 0.0));
+  }
+
+  void _handleFlingAnimation() {
+    setState(() {
+      _offset = _flingAnimation.value;
+    });
+  }
+
+  void _handleOnScaleStart(ScaleStartDetails details) {
+    setState(() {
+      _previousScale = _scale;
+      _normalizedOffset = (details.focalPoint - _offset) / _scale;
+      // The fling animation stops if an input gesture starts.
+      _controller.stop();
+    });
+  }
+
+  void _handleOnScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _scale = (_previousScale * details.scale).clamp(1.0, 4.0);
+      // Ensure that image location under the focal point stays in the same place despite scaling.
+      _offset = _clampOffset(details.focalPoint - _normalizedOffset * _scale);
+    });
+  }
+
+  void _handleOnScaleEnd(ScaleEndDetails details) {
+    final double magnitude = details.velocity.pixelsPerSecond.distance;
+    if (magnitude < _kMinFlingVelocity)
+      return;
+    final Offset direction = details.velocity.pixelsPerSecond / magnitude;
+    final double distance = (Offset.zero & context.size).shortestSide;
+    _flingAnimation = _controller.drive(Tween<Offset>(
+        begin: _offset,
+        end: _clampOffset(_offset + direction * distance)
+    ));
+    _controller
+      ..value = 0.0
+      ..fling(velocity: magnitude / 1000.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onScaleStart: _handleOnScaleStart,
+      onScaleUpdate: _handleOnScaleUpdate,
+      onScaleEnd: _handleOnScaleEnd,
+      child: ClipRect(
+        child: Transform(
+          transform: Matrix4.identity()
+            ..translate(_offset.dx, _offset.dy)
+            ..scale(_scale),
+          child: Hero(
+            tag: widget.photo.tag,
+            child: Image.network(
+              widget.photo.imageSrc,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
